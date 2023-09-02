fc3a45b6fe02d4a971b46b30b5ab1465d67f3628
John Serrano <andrey_201015@hotmail.com>
2023-09-02 16:25:24 -0500

    add styles and button get new cat

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import type { MouseEventHandler } from 'react'
 import './App.css'
 
 const URL_FACT = 'https://catfact.ninja/fact'
@@ -6,9 +7,9 @@ const URL_IMAGE_CAT = 'https://cataas.com'
 
 function App (): JSX.Element {
   const [fact, setFact] = useState<null | string>(null)
-  const [urlImage, setUrlImage] = useState<null | string>(null)
+  const [urlImage, setUrlImage] = useState<null | string>(fact)
 
-  const getFact = async (): Promise<void> => {
+  const getFact = (): void => {
     fetch(URL_FACT)
       .then(async (res) => {
         if (!res.ok) {
@@ -19,9 +20,9 @@ function App (): JSX.Element {
       })
       .then(({ fact }: { fact: string }) => {
         // console.log({ fact })
-        const firstWord = fact.split(' ').slice(0, 3).join(' ')
+        const firstWord = fact.split(' ', 3).join(' ')
         console.log(firstWord)
-        setFact(firstWord)
+        // setFact(firstWord)
         getCat({ fact: firstWord })
       })
       .catch(err => {
@@ -30,8 +31,6 @@ function App (): JSX.Element {
   }
 
   const getCat = ({ fact }: { fact: string }): void => {
-    console.log(fact)
-
     fetch(`${URL_IMAGE_CAT}/cat/says/${fact}?json=true`)
       .then(async (res) => {
         if (!res.ok) {
@@ -43,6 +42,7 @@ function App (): JSX.Element {
       .then(({ url }) => {
         console.log({ url })
         setUrlImage(url)
+        setFact(fact)
       })
       .catch(err => {
         console.log(err)
@@ -50,12 +50,22 @@ function App (): JSX.Element {
   }
 
   useEffect(() => {
-    getFact().catch(err => { console.log(err) })
+    if (fact == null) return
+
+    getFact()
+    // console.log('load...')
   }, [])
+
+  const getNewCat: MouseEventHandler<HTMLButtonElement> = () => {
+    getFact()
+  }
+
   return (
     <main>
-      <h2>La palabra es: {fact}</h2>
+      {fact != null && <h2 className='fact'>This is a cat fact: {fact}</h2>}
       {urlImage != null && <img src={`${URL_IMAGE_CAT}/${urlImage}`} alt={urlImage} />}
+
+      <button onClick={getNewCat} className='getCat'>Get New Cat</button>
     </main>
   )
 }
